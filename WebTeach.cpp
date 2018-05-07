57e34798308b9f9d1ffe52be74ccdb0d8b204c98
Lin Zhirong <lin.zr.thu@live.cn>
2018-05-07 19:36:22 +0800

    main struct

--- WebTeach.cpp
+++ WebTeach.cpp
@@ -1,10 +1,44 @@
 #include "stdafx.h"
 using namespace std;
+void SetupServer(char *IPStr);
+
+void WebTest(char *IP)
+{
+	SetupServer(IP);
+
+sockaddr_in addrClient;
+int iLength=sizeof(sockaddr_in);
+SOCKET sClient=accept(sServer,(sockaddr *)&addrClient,&iLength);
+char buf[2048];
+int buflen;
+buflen=recv(sClient,buf,2047,0);
+if(buflen>0)
+{
+	buf[buflen]='\0';
+	cout<<buf<<endl;
+}
+send(sClient,
+	 longstr,
+	 sizeof(longstr),0);
+	
+	closesocket(sClient);
+	closesocket(sServer);
+	WSACleanup();
+}
 
 int main(int argc, char* argv[])
 {
 	int ret=EOF;
 
+	// load data base
+	LoadDataBase();
+	// initial server
+	SetupServer(argv[1]);
+	while(true)
+	{
+		// get request line
+		// analyse and response
+	}
+
 	return ret;
 }
-
